6161473a93e4602b8acb85065696617dee851eea
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-11 18:33:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,37 +1,31 @@
 class BankAccount
+  attr_accessor :balance, :status
   attr_reader :name
-  attr_accessor :balance, :status, :account_hash
 
-  def initialize(name)
+  def initialize(name, balance=1000, status="open")
     @name = name
-    @balance = 1000
-    @status = "open"
+    @balance = balance
+    @status = status
   end
 
-  def deposit(deposit_amount)
-    @balance += deposit_amount
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def withdrawal(amount)
+    self.balance -= amount
   end
 
   def display_balance
-    p "Your balance is $#{self.balance}."
-  end
-
-  def status=(status)
-    @status
-  end
-
-  def balance=(balance)
-    @balance
+    "Your balance is $#{self.balance}."
   end
 
   def valid?
     self.status == "open" && self.balance > 0 ? true : false
   end
 
-  def account_hash
-    instance_variables.map do |var|
-        [var[1..-1].to_sym, instance_variable_get(var)]
-    end.to_h
+  def close_account
+    self.status = "closed"
   end
 
 end
